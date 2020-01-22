263e11acf2e6fed8ebefa483c0daf7026193dcd9
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-22 13:19:55 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
-
+  if speed.between?(40,60)
+    false
+  else
+    true
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+# speed < 40 || speed > 60 ? true : false
+  speed.between?(40,60) ? false : true
 end
-	
-
-
